07ffa161e83c2fdc6f43c9561e6fc3d96d5ccfab
VONGOCMINHTRANG <vongocminhtrang2712@gmail.com>
2023-12-14 10:34:37 +0700

    [#2] Set up routing

---
+++ src/app/pages/tests/tests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tests',
+  templateUrl: './tests.component.html',
+  styleUrl: './tests.component.scss'
+})
+export class TestsComponent {
+
+}
