0a5ce76b274f14090cea482b213a624709b6bcb8
VONGOCMINHTRANG <vongocminhtrang2712@gmail.com>
2023-12-15 16:38:26 +0700

    [#9] Handle blur when opening menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss', '../../app.component.scss'],
 })
 export class HomeComponent {
+  @ViewChild('contentWrapper') contentWrapper!: ElementRef;
 
+  addClassBlur(isOpenMenu: boolean) {
+    if (isOpenMenu) {
+      this.contentWrapper.nativeElement.classList.add('blur');
+    } else {
+      this.contentWrapper.nativeElement.classList.remove('blur');
+    }
+  }
 }
