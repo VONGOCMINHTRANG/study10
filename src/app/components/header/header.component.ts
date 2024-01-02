88bb4bc8c3ca91cd0931f35c7ebc0da8aa240ddd
VONGOCMINHTRANG <vongocminhtrang2712@gmail.com>
2024-01-02 11:51:23 +0700

    [#11] Handle menu when resize window and routing for page not found

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
+import { EventManager } from '@angular/platform-browser';
 import {
   Component,
   Output,
   ViewChild,
   EventEmitter,
   ElementRef,
+  HostListener,
 } from '@angular/core';
 import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 
@@ -14,9 +16,16 @@ import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
 })
 export class HeaderComponent {
   @Output() handleMenu = new EventEmitter<boolean>();
-  @ViewChild(MatMenuTrigger) clickHoverMenuTrigger!: MatMenuTrigger;
   @ViewChild('container') container!: ElementRef;
   @ViewChild('menu') menu!: MatMenu;
+  @ViewChild(MatMenuTrigger) clickHoverMenuTrigger!: MatMenuTrigger;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    // Nếu từ 993px trở lên, thì đóng menu!: MatMenu
+    if (event.target.innerWidth >= 993) {
+      this.clickHoverMenuTrigger.closeMenu();
+    }
+  }
 
   menuOpened(isOpenMenu: boolean) {
     this.handleMenu.emit(isOpenMenu);
